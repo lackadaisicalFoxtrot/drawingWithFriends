9f6015a00e0b59b15facaa468764d1ae0ead03f8
Xue Hu <dxuehu@gmail.com>
2015-06-18 16:03:26 -0700

    start emits and change bower json

--- server/server.js
+++ server/server.js
@@ -7,12 +7,17 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 //will change port at later date
 var port = 8080;
+server.listen(port);
 
 app.use(express.static(__dirname + '/../client'));
-
-io.on('connection', function(client) {
+io.on('connection', function(socket) {
     console.log('Client connected...');
-    console.log(client);
+    console.log(socket);
+    socket.broadcast.emit('user broadcasted');
+    
+    socket.on('disconnect', function() {
+      io.emit('user disconnected'); //custom event
+    });
+
 });
 
-server.listen(port);
