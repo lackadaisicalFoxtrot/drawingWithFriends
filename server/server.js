f5aa71c35e878de4c917498505807ea32bc0b44e
Xue Hu <dxuehu@gmail.com>
2015-06-18 15:39:06 -0700

    move assets around

--- server/server.js
+++ server/server.js
@@ -3,16 +3,15 @@ var util = require('./utils');
 //var db = require('./db/config.js');
 
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-app.use(express.static(__dirname + '/../client'));
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+//will change port at later date
+var port = 8080;
 
+app.use(express.static(__dirname + '/../client'));
 
 io.on('connection', function(client) {
     console.log('Client connected...');
 });
 
-//will change port at later date
-var port = 8080;
-
-http.listen(port);
+server.listen(port);
