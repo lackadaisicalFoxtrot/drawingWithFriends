a703c8c6cd85e2e665b05f9fe308ff4e7654cd10
Medhir Bhargava <medhirbhargava@gmail.com>
2015-06-17 19:09:39 -0700

    Scaffold front end

---
+++ client/js/models/AppModel.js
@@ -0,0 +1,6 @@
+// AppModel.js
+// Not sure how necessary this model will be... will possibly be responsible for events triggered by a picture model. 
+
+var app = app || {};
+
+app.AppModel = Backbone.Model.extend({});
---
+++ client/js/views/AppView.js
@@ -0,0 +1,7 @@
+// AppView.js
+// This view will be responsible for instantiating the entire app. Other views will be instantiated on creation of this view. 
+
+var app = app || {};
+
+app.AppView = Backbone.View.extend({});
+
