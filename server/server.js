9988144301ba3b19c33713e57d2d6e00cc36e13b
Liam Gallivan <gallivanster@gmail.com>
2015-06-18 18:10:07 -0700

    Added GET request for all pictures

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var util = require('./utils');
-//var db = require('./db/config.js');
+var db = require('./db/config');
+var Lines =require('./db/collections/lines');
+var Pictures =require('./db/collections/pictures');
 
 var app = express();
 var server = require('http').Server(app);
@@ -20,4 +22,18 @@ io.on('connection', function(socket) {
     });
 
 });
-
+app.route('/gallery')
+	.get(function(req,res,next){
+		var results = [];
+		new Pictures({}).fetch().then(function(pictures){
+			for(var i = 0; i<pictures.models.length; i++){
+				results[i]=[];
+				new Lines({picture_id : pictures.get(i).get('id')}).fetch().then(function(lines){
+					for (var i = 0; i < lines.models.length; i++) {
+						results[i].push(lines.get(i));
+					}
+				});
+			}
+		});
+		res.send(results);
+	});
