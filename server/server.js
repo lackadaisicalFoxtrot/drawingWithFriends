a8a39bfb5d0c8919bcb564cb539b138555e27a23
Xue Hu <dxuehu@gmail.com>
2015-06-18 20:22:31 -0700

    get basic user mouse movement broadcasting to work

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var util = require('./utils');
-var db = require('./db/config');
-var Lines =require('./db/collections/lines');
-var Pictures =require('./db/collections/pictures');
+//var db = require('./db/config');
+//var Lines =require('./db/collections/lines');
+//var Pictures =require('./db/collections/pictures');
 
 var app = express();
 var server = require('http').Server(app);
@@ -16,11 +16,13 @@ io.on('connection', function(socket) {
     console.log('Client connected...');
     console.log(socket);
     socket.broadcast.emit('user broadcasted');
-
+    socket.on('user moved', function(data) {
+      console.log('a user moved. their coords: ', data);
+      socket.broadcast.emit('user moved', data);
+    });
     socket.on('disconnect', function() {
       io.emit('user disconnected'); //custom event
     });
-
 });
 app.route('/gallery')
 	.get(function(req,res,next){
