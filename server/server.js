878a41420194b67c47b92f04c034a0514210bb59
Xue Hu <dxuehu@gmail.com>
2015-06-24 03:20:23 -0700

    Cleanup a bit

--- server/server.js
+++ server/server.js
@@ -3,24 +3,15 @@ var util = require('./utils');
 var db = require('./db/config');
 var Line = require('./db/models/line');
 var Lines = require('./db/collections/lines');
-//var Promise = require('bluebird');
-//var Lines =require('./db/collections/lines');
 //var Pictures =require('./db/collections/pictures');
 
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-//will change port at later date
 var port = 8080;
 server.listen(port);
 
 app.use(express.static(__dirname + '/../client'));
-//Lines.save().then(function(res) { console.log('saved. ', res); });
-
-//var line = new Line({coordinates: JSON.stringify([4,5])});
-//line.save().then(function(res) {
-  //console.log('saved ', res);
-//});
 
 var timerStarted = false;
 
@@ -38,7 +29,6 @@ var savePictureAndReset = function(socket) {
     timerStarted = false;
     console.log('set timer to false');
   });
-
   //Lines is one picture
   //Lines.invokeThen('save', null, options).then(function(res) {
     //// ... all models in the collection have been saved
@@ -62,7 +52,6 @@ io.on('connection', function(socket) {
     //but on the server we keep the id until we store it in the db which is much longer--thus we def
     //need no collisions or we overwrite some lines
     Lines.add({id: data.id, coordinates: data.coordinates}, {merge: true});
-    //saveToDb();
     //if (!timerStarted) {
       //setTimeout(savePictureAndReset.bind(null, socket), 5000); //every 5 seconds
       //timerStarted = true;
@@ -70,8 +59,6 @@ io.on('connection', function(socket) {
     //}
     //setTimeout(saveToDb, 1000*60*5); //save to db every 5 minutes. begin this timer when at least 1 person has started drawing
 
-    //now save the data to a bookshelf collection of lines ie a picture
-    //that persists here. when the pic is done save() to the db
     //if (data.id) { //server has seen line before, it is an existing line someone is drawing
     ////do something 
     //} else { //this is the start of a new line
@@ -80,7 +67,6 @@ io.on('connection', function(socket) {
     ////broadcast with a particular id
     //});
     //}
-    //socket.broadcast.emit('user moved', data);
     socket.broadcast.emit('user moved', data);
   });
   socket.on('user ended', function(data) {
