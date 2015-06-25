71af13a2e1751d3c8415d299ae62382e2b29a235
Medhir Bhargava <medhirbhargava@gmail.com>
2015-06-25 10:27:41 -0700

    Correctly render timer

--- client/js/views/TimerView.js
+++ client/js/views/TimerView.js
@@ -5,17 +5,21 @@ var app = app || {};
 app.TimerView = Backbone.View.extend({
 
   tagName: 'div', 
-  className: 'timerContainer'
+  className: 'timerContainer',
 
   initialize: function() {
-    this.model.on('tick', this.render);
+    this.model.on('start', this.attach, this);
+    this.model.on('tick', this.render, this);
   }, 
 
   template: _.template('<h2><%= time %></h2>'), 
 
+  attach: function() {
+    $('.container').append(this.render());
+  }, 
+
   render: function() {
     this.$el.children().detach();
-
-    return this.$el.html(this.template(this.model.attributes));
+    return this.$el.append(this.template(this.model.attributes));
   }
 });
