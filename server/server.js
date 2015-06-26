78171c4775d7a32c31c028b0fad8225f5ada1cb7
Xue Hu <dxuehu@gmail.com>
2015-06-26 02:24:31 -0700

    Clean up server.js, start moving helper fns out

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 var express = require('express');
-var util = require('./utils');
 var db = require('./db/config');
 var Line = require('./db/models/line');
 var Lines = require('./db/collections/lines');
-var Picture =require('./db/models/picture');
-var Pictures =require('./db/collections/pictures');
+var Picture = require('./db/models/picture');
+var Pictures = require('./db/collections/pictures');
+var util = require('./utils');
+var routes = require('./routes');
 
 //timer functionality
 var Timer = require('timer-stopwatch');
+
 
 var app = express();
 var server = require('http').Server(app);
@@ -16,6 +18,7 @@ var port = 8080;
 server.listen(port);
 
 app.use(express.static(__dirname + '/../client'));
+require('./routes')(app); //is this how to decorate/inject?
 
 var timer = null;
 
@@ -23,50 +26,15 @@ var timer = null;
 //Lines.add({id:4949, coordinates: [10,2]});
 //Lines.add({id:6534, coordinates: [7,49]});
 
-//this saves the picture and lines to the db, then clears the Lines collection (that's all!) and sets the timer to null
-var savePictureAndReset = function(socket) {
-  if (Lines.length < 1) return; //make sure we are not creating a new picture if there are no lines. this fn shouldn't be called anyway in that case...
-
-  new Picture({}).save().then(function(picture) { //save first to get a picture id
-    var pic_lines = picture.lines();
-    Lines.mapThen(function(model) { //iter through lines, prepare attributes for saving to db, then save to db with create
-      model.unset('id', {silent: true}); //silent = don't fire 'change' event. less overhead?
-      model.set('coordinates', JSON.stringify(model.get('coordinates'))); //shouldn't stringifcation be automatic on save?? hm
-      return pic_lines.create(model.attributes); //eww exposing model.attr but trying the bookshelf relation way
-
-      //Other methods tried:
-      //pic_lines.add(model); //something like this would be nice
-      //model.picture().set('id', picture.get('id')); //another method, less encapsulatey, setting the relationship via the other direction
-      //model.set('picture_id', picture.get('id')); same as above but even more manual and not using bookshelf relationship
-      //return model.save(); //create does this above
-
-    }).then(function(res) {
-      Lines.reset();
-      socket.emit('got lines', Lines); //client redraws lines (no lines) TODO
-      //timerStarted = false;
-      timer = null;
-    }).catch(function(err) {
-      return console.error('error saving all line models to db: ', err);
-    });
-  }).catch(function(err) {
-    return console.error('error saving picture to db: ', err);
-  });
-};
-
-var sendTimer = function(socket) { //send the timer upon (any) client timer mode init (if there is a timer going), or upon user drawing the first line. io.emit to all to keep client timer more up to date
-  //if there is no timer going, will emit time: null
-  io.emit('setTimer', {time: timer && timer.ms});
-};
-
 io.on('connection', function(socket) {
-
+  //TODO rename these events without spaces etc
   socket.on('get lines', function() { //user has requested the lines because the picture view is rendering
     socket.emit('got lines', Lines); //send the lines every time the user requests it (not very efficient, think about having view run in bg so this is only emitted once)
     //maybe call this set lines to conform
   });
 
   socket.on('getTimer', function() {
-    sendTimer(socket);
+    util.sendTimer(io, timer);
   });
 
   socket.on('user moved', function(data) {
@@ -74,22 +42,22 @@ io.on('connection', function(socket) {
 
     Lines.add({id: data.id, coordinates: data.coordinates}, {merge: true});
 
+    //TODO move timer logic to utils?
     if (!timer) {
       var ms = 300000; //5 min
       timer = new Timer(30000, { //30 sec
-          refreshRateMS: '5000', //??set the interval for when savePictureAndReset is called
-          almostDoneMS: 290000 //this will emit an event when the timer is almost done ... probably not necessary, TODO - get rid of this if we don't use it
-        }); //set new timer for 5 minutes
+          refreshRateMS: '1000'
+          //almostDoneMS: 290000 //this will emit an event when the timer is almost done ... probably not necessary, TODO - get rid of this if we don't use it
+        });
 
-      timer.on('time', function(time) {
-      });
+      //timer.on('time', function(time) {
+      //});
       timer.on('done', function() {
         //save entire picture to DB
-        savePictureAndReset(socket); 
+        util.savePictureAndReset(socket);
       });
       timer.start();
-      //console.log('timer started');
-      sendTimer(socket);
+      util.sendTimer(io, timer);
     }
     socket.broadcast.emit('user moved', data);
   });
@@ -103,18 +71,3 @@ io.on('connection', function(socket) {
   //});
 });
 
-app.route('/gallery')
-.get(function(req,res,next){
-  var results = [];
-  new Pictures({}).fetch().then(function(pictures){
-    for(var i = 0; i<pictures.models.length; i++){
-      results[i]=[];
-      new Lines({picture_id : pictures.get(i).get('id')}).fetch().then(function(lines){ //TODO Lines is already an instance of a collection so this won't work
-        for (var i = 0; i < lines.models.length; i++) {
-          results[i].push(lines.get(i));
-        }
-      });
-    }
-  });
-  res.send(results);
-});
--- server/utils.js
+++ server/utils.js
@@ -1,12 +1,43 @@
 // will contain and export callback functions for server actions
-var count = 0;
-module.exports.serverId = function(cb) {
-  //instantiate new line, save the line to db
-  //get the line in db's id to serve back.
-  //http get request that pings server
-  //server uses bookshelf to talk to db
-  //dummy id for now
-  cb(++count);
+var db = require('./db/config');
+var Line = require('./db/models/line');
+var Lines = require('./db/collections/lines');
+var Picture = require('./db/models/picture');
+var Pictures = require('./db/collections/pictures');
+
+module.exports.sendTimer = function(io, timer) { //send the timer upon (any) client timer mode init (if there is a timer going), or upon user drawing the first line. io.emit to all to keep client timer more up to date
+  //if there is no timer going, will emit time: null
+  io.emit('setTimer', {time: timer && timer.ms});
 };
+
+//this saves the picture and lines to the db, then clears the Lines collection (that's all!) and sets the timer to null
+module.exports.savePictureAndReset = function(socket) {
+  if (Lines.length < 1) return; //make sure we are not creating a new picture if there are no lines. this fn shouldn't be called anyway in that case...
+
+  new Picture({}).save().then(function(picture) { //save first to get a picture id
+    var pic_lines = picture.lines();
+    Lines.mapThen(function(model) { //iter through lines, prepare attributes for saving to db, then save to db with create
+      model.unset('id', {silent: true}); //silent = don't fire 'change' event. less overhead?
+      model.set('coordinates', JSON.stringify(model.get('coordinates'))); //shouldn't stringifcation be automatic on save?? hm
+      return pic_lines.create(model.attributes); //eww exposing model.attr but trying the bookshelf relation way
+
+      //Other methods tried:
+      //pic_lines.add(model); //something like this would be nice
+      //model.picture().set('id', picture.get('id')); //another method, less encapsulatey, setting the relationship via the other direction
+      //model.set('picture_id', picture.get('id')); same as above but even more manual and not using bookshelf relationship
+      //return model.save(); //create does this above
+
+    }).then(function(res) {
+      Lines.reset();
+      socket.emit('got lines', Lines); //client redraws lines (no lines) TODO
+      timer = null;
+    }).catch(function(err) {
+      return console.error('error saving all line models to db: ', err);
+    });
+  }).catch(function(err) {
+    return console.error('error saving picture to db: ', err);
+  });
+};
+
 
 
