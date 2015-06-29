2f7690e672fb064b948d71c44777fb8cf69a7769
Xue Hu <dxuehu@gmail.com>
2015-06-29 11:11:45 -0700

    Work on styles

--- client/js/views/HomeView.js
+++ client/js/views/HomeView.js
@@ -9,6 +9,7 @@ app.HomeView = Backbone.View.extend({
   render : function () {
     this.$el.detach();
     $('.container')
-    .append('<div class="home"><h1> Drawing With Friends</h1><p> Join friends and strangers in creating a sketch. Will you draw a cat, a house, the Mona Lisa? Click below to start drawing</p><a href="#draw">Draw</a></div>');
+    .append('<p> Join friends and strangers in creating a sketch. \
+            Will you draw a cat, a house, the Mona Lisa?</p>');
   }
 });
