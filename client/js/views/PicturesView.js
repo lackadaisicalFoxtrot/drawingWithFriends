92bc2cc9836e882b99ca95f0a786b7c590b5de1e
JamesKennethRobinson <Jameskennthrobinson@gmail.com>
2015-06-29 12:36:11 -0700

    gallery working

--- client/js/views/PicturesView.js
+++ client/js/views/PicturesView.js
@@ -8,6 +8,11 @@ app.PicturesView = Backbone.View.extend({
   className: 'canvas',
   collection : app.PicturesCollection,
 
+  svgLine: d3.svg.line()
+  .x(function(d) { return d[0]; })
+  .y(function(d) { return d[1]; })
+  .interpolate('basis'),
+
   initialize : function() {
     this.collection = new app.PicturesCollection();
     //solving our async problems with set timeout is the most hackey hackish thing I have ever done
@@ -21,12 +26,20 @@ app.PicturesView = Backbone.View.extend({
     var data = this.collection.modelData;
 
     for (var pic in data){
-      this.container.append(this.tagName)
+      var svg = this.container.append(this.tagName)
       .attr({
         'class': this.className,
         width: 500,
         height: 500
       });
+      for (var i = 0; i < data[pic].length; i++) {
+        console.log(JSON.parse(data[pic][i]));
+        var line = svg.append('path')
+        .datum(JSON.parse(data[pic][i]))
+        .attr('class', 'line');
+        line.attr('d', this.svgLine);
+      }
+
     }
   }
 });
